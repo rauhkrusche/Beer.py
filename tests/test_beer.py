71c782f78760f11d98a4338c493b5b6c1caa438b
Benjamin Leuckefeld <code@deratrox.de>
2016-05-16 15:38:18 +0200

    fixed test

--- tests/test_beer.py
+++ tests/test_beer.py
@@ -8,13 +8,15 @@ class TestBeer(unittest.TestCase):
 
         self.assertEqual(serialized, "BEER∫BEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEER∫µ∫µµµµµµµµµµµµµµµµµµµµµµµµµµ∫BEER-BEER∫BEERBEERBEER∫E-")
 
-    def test_serialize_beer(self):
+    def test_deserialize_beer(self):
         deserialized = Beer.deserialize_beer("BEER∫BEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEERBEER∫µ∫µµµµµµµµµµµµµµµµµµµµµµµµµµ∫BEER-BEER∫BEERBEERBEER∫E-")
 
         self.assertEqual(deserialized, "qmQM.eE-")
 
-    def test_loop(self):
-        self.assertEqual("QPqpALalYMymberBER,.-12#", Beer.deserialize_beer(Beer.serialize_beer("QPqpALalYMymberBER,.-12#")))
+    def test_loop_beer(self):
+        deserialized = Beer.deserialize_beer(Beer.serialize_beer("QPqpALalYMymberBER,.-12#"))
+
+        self.assertEqual(deserialized, "QPqpALalYMymberBER,.-12#")
 
 
 if __name__ == '__main__':
